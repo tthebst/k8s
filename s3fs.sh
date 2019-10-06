0390fac2e13c4b67fb9307cc1f7a1d94443857b0
tthebst <gretler.tim@gmail.com>
2019-10-06 20:02:37 +0200

    updated s3fs script

--- s3fs.sh
+++ s3fs.sh
@@ -10,9 +10,9 @@ chmod 600 ${HOME}/.passwd-s3fs
 mkdir ${HOME}/clusterdata
 s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs  -o url="https://s3-eu-central-1.amazonaws.com"
 
-cp ${HOME}/.passwd-s3fs pi@192.168.1.99:~/
-cp ${HOME}/.passwd-s3fs pi@192.168.1.98:~/
-cp ${HOME}/.passwd-s3fs pi@192.168.1.97:~/
+scp ${HOME}/.passwd-s3fs pi@192.168.1.99:~/
+scp ${HOME}/.passwd-s3fs pi@192.168.1.98:~/
+scp ${HOME}/.passwd-s3fs pi@192.168.1.97:~/
 
 
 ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
