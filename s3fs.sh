6b0508b72805c417f08ecd0bce9da130c0c31584
tthebst <gretler.tim@gmail.com>
2019-10-06 18:59:05 +0200

    updated s3fs script

--- s3fs.sh
+++ s3fs.sh
@@ -9,11 +9,11 @@ echo -n $1:$2 > ${HOME}/.passwd-s3fs
 chmod 600 ${HOME}/.passwd-s3fs
 mkdir ${HOME}/clusterdata
 
-s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs
+s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs  -o url="https://s3-eu-central-1.amazonaws.com"
 
 
 
-ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs  -o dbglevel=info -f -o curldbg;exit'
-ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs;exit'
-ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs;exit'
+ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
+ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
+ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n $1:$2 > ${HOME}/.passwd-s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
 
