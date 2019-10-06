fd412d887e38ff41f7dafd43ca64d28a40c46ea8
tthebst <gretler.tim@gmail.com>
2019-10-06 17:03:47 +0200

    updated s3fs script

--- s3fs.sh
+++ s3fs.sh
@@ -4,14 +4,14 @@
 #install on master node
 sudo apt-get install -y s3fs
 
-echo $1:$2 > ${HOME}/.passwd-s3fs
-sudo chmod 600 ${HOME}/.passwd-s3fs
-mkdir ${HOME}/k8s_data
-s3fs raspberrycluster ${HOME}/k8s_data -o passwd_file=${HOME}/.passwd-s3fs
+sudo rm ${HOME}/.passwd-s3fs
+echo -n $1:$2 > ${HOME}/.passwd-s3fs
+mkdir ${HOME}/k8s_data1
+s3fs raspberrycluster ${HOME}/k8s_data1 -o passwd_file=${HOME}/.passwd-s3fs
 
 
 
-ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;sudo chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data;s3fs raspberrycluster ${HOME}/k8s_data -o passwd_file=${HOME}/.passwd-s3fse;exit'
-ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;sudo chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data;s3fs raspberrycluster ${HOME}/k8s_data -o passwd_file=${HOME}/.passwd-s3fse;exit'
-ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;sudo chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data;s3fs raspberrycluster ${HOME}/k8s_data -o passwd_file=${HOME}/.passwd-s3fse;exit'
+ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data1;s3fs raspberrycluster ${HOME}/k8s_data1 -o passwd_file=${HOME}/.passwd-s3fse;exit'
+ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data1;s3fs raspberrycluster ${HOME}/k8s_data1 -o passwd_file=${HOME}/.passwd-s3fse;exit'
+ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;sudo rm ${HOME}/.passwd-s3fs;echo -n  $1:$2 > ${HOME}/.passwd-s3fs;mkdir ${HOME}/k8s_data1;s3fs raspberrycluster ${HOME}/k8s_data1 -o passwd_file=${HOME}/.passwd-s3fse;exit'
 
