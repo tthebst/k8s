297a3f6f17418ccfc831cfd0a0f16f2ec37f8737
tthebst <gretler.tim@gmail.com>
2019-10-06 16:34:55 +0200

    updated s3fs script

--- s3fs.sh
+++ s3fs.sh
@@ -5,30 +5,43 @@
 sudo apt-get install -y s3fs
 
 echo $1:$2 > ${HOME}/.passwd-s3fs
-
+sudo chmod 600 ${HOME}/.passwd-s3fs
+mkdir ${HOME}/data_cluster
 s3fs mybucket ${HOME}/data_cluster -o passwd_file=${HOME}/.passwd-s3fs
+
 
 
 ssh pi@192.168.1.99
 
 sudo apt-get install -y s3fs
 
-echo $1:$2 > ${HOME}/.passwd-s3fs
+exit
 
+echo $1:$2 > ${HOME}/.passwd-s3fs
+sudo chmod 600 ${HOME}/.passwd-s3fs
+mkdir ${HOME}/data_cluster
 s3fs mybucket ${HOME}/data_cluster -o passwd_file=${HOME}/.passwd-s3fs
 
 ssh pi@192.168.1.98
 
 sudo apt-get install -y s3fs
 
-echo $1:$2 > ${HOME}/.passwd-s3fs
+exit
 
+echo $1:$2 > ${HOME}/.passwd-s3fs
+sudo chmod 600 ${HOME}/.passwd-s3fs
+mkdir ${HOME}/data_cluster
 s3fs mybucket ${HOME}/data_cluster -o passwd_file=${HOME}/.passwd-s3fs
+
+exit
 
 ssh pi@192.168.1.97
 
 sudo apt-get install -y s3fs
 
 echo $1:$2 > ${HOME}/.passwd-s3fs
-
+sudo chmod 600 ${HOME}/.passwd-s3fs
+mkdir ${HOME}/data_cluster
 s3fs mybucket ${HOME}/data_cluster -o passwd_file=${HOME}/.passwd-s3fs
+
+exit
