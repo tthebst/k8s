0ee8b381d3d6fc6afb079b6f5853321197e41c77
tthebst <gretler.tim@gmail.com>
2019-10-07 11:27:44 +0200

    first try with bitcoin

--- s3fs.sh
+++ s3fs.sh
@@ -4,6 +4,9 @@
 #install on master node
 sudo apt-get install -y s3fs
 
+
+sudo unmount ../clusterdata/
+sudo rm -r ../clusterdata/
 sudo rm ${HOME}/.passwd-s3fs
 echo -n $1:$2 > ${HOME}/.passwd-s3fs
 chmod 600 ${HOME}/.passwd-s3fs
@@ -15,7 +18,7 @@ scp ${HOME}/.passwd-s3fs pi@192.168.1.98:~/
 scp ${HOME}/.passwd-s3fs pi@192.168.1.97:~/
 
 
-ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
-ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
-ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
+ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;sudo unmount clusterdata/;sudo rm -r clusterdata;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
+ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;sudo unmount clusterdata/;sudo rm -r clusterdata;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
+ssh pi@192.168.1.97 'sudo apt-get install -y s3fs;sudo unmount clusterdata/;sudo rm -r clusterdata;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
 
