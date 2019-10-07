b300a66185c8e58a71feec12bc14622d21d63831
tthebst <gretler.tim@gmail.com>
2019-10-07 11:34:08 +0200

    first try with bitcoin

--- s3fs.sh
+++ s3fs.sh
@@ -7,16 +7,23 @@ sudo apt-get install -y s3fs
 
 sudo umount ../clusterdata/
 sudo rm -r ../clusterdata/
+sudo rm -r /etc/fuse.conf
+sudo cp ./fuse.conf /etc/
 sudo rm ${HOME}/.passwd-s3fs
 echo -n $1:$2 > ${HOME}/.passwd-s3fs
 chmod 600 ${HOME}/.passwd-s3fs
 mkdir ${HOME}/clusterdata
 s3fs raspberrycluster ${HOME}/clusterdata -o passwd_file=${HOME}/.passwd-s3fs  -o url="https://s3-eu-central-1.amazonaws.com"
 
+
+
+
 scp ${HOME}/.passwd-s3fs pi@192.168.1.99:~/
 scp ${HOME}/.passwd-s3fs pi@192.168.1.98:~/
 scp ${HOME}/.passwd-s3fs pi@192.168.1.97:~/
-
+scp ./fuse.conf pi@192.168.1.99:/etc/fuse.conf
+scp ./fuse.conf pi@192.168.1.98:/etc/fuse.conf
+scp ./fuse.conf pi@192.168.1.97:/etc/fuse.conf
 
 ssh pi@192.168.1.99 'sudo apt-get install -y s3fs;sudo umount clusterdata/;sudo rm -r clusterdata;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
 ssh pi@192.168.1.98 'sudo apt-get install -y s3fs;sudo umount clusterdata/;sudo rm -r clusterdata;chmod 600 ${HOME}/.passwd-s3fs;mkdir ${HOME}/clusterdata;s3fs raspberrycluster ${HOME}/clusterdata -o allow_other -o passwd_file=${HOME}/.passwd-s3fs -o url="https://s3-eu-central-1.amazonaws.com";exit'
