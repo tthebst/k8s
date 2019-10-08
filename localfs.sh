80d2adde390ab546cae90fb8acd083b214df4683
tthebst <gretler.tim@gmail.com>
2019-10-08 21:31:05 +0200

    first try with bitcoin

--- localfs.sh
+++ localfs.sh
@@ -9,9 +9,9 @@ ssh 192.168.1.97 'sudo apt-get install exfat-fuse exfat-utils;sudo apt install n
 mkdir ~/localfs  
 sudo sudo apt-get install exfat-fuse exfat-utils
 sudo umount ~/localfs
-sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs
+sudo mount -t exfat -ouid=1001,gid=1001 -o umask=0000 -o allow_other   192.168.1.97:/home/pi/localfs ~/localfs
 echo '192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0' >>sudo /etc/fstab
-ssh 192.168.1.98 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
-ssh 192.168.1.99 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.98 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -t exfat -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.99 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -t exfat -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
 mount -t nfs
-
+sudo cat /etc/fstab
