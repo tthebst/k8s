a5468b7235c52aebccc4f24787952d10aa297743
tthebst <gretler.tim@gmail.com>
2019-10-08 21:10:38 +0200

    first try with bitcoin

--- localfs.sh
+++ localfs.sh
@@ -6,8 +6,8 @@
 scp ./exports 192.168.1.97:~/
 #connect to worker1 and add localfs to exports
 ssh 192.168.1.97 'sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs'
-mount 192.168.1.97:/home/pi/localfs /localfs
-ssh 192.168.1.98 'mount 192.168.1.97:/home/pi/localfs /localfs'
-ssh 192.168.1.99 'mount 192.168.1.97:/home/pi/localfs /localfs'
+sudo mount 192.168.1.97:/home/pi/localfs /localfs
+ssh 192.168.1.98 'sudo mount 192.168.1.97:/home/pi/localfs /localfs'
+ssh 192.168.1.99 'sudo mount 192.168.1.97:/home/pi/localfs /localfs'
 mount -t nfs
 
