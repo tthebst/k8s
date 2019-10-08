8f8cadf2a6305c007d862938f69f8583a795d894
tthebst <gretler.tim@gmail.com>
2019-10-08 21:25:18 +0200

    first try with bitcoin

--- localfs.sh
+++ localfs.sh
@@ -7,10 +7,10 @@ scp ./exports 192.168.1.97:~/
 #connect to worker1 and add localfs to exports
 ssh 192.168.1.97 'sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs'
 mkdir ~/localfs  
-sudo umount 192.168.1.97:/home/pi/localfs ~/localfs
+sudo umount ~/localfs
 sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs
 echo '192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0' >>sudo /etc/fstab
-ssh 192.168.1.98 'mkdir ~/localfs ;sudo umount 192.168.1.97:/home/pi/localfs ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
-ssh 192.168.1.99 'mkdir ~/localfs ;sudo umount 192.168.1.97:/home/pi/localfs ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.98 'mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.99 'mkdir ~/localfs ;sudo umount ~/localfs;sudo mount -ouid=1001,gid=1001 -o umask=0000 -o allow_other 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
 mount -t nfs
 
