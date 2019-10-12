aa53015f238ca6c23f75430145f35a8773d82e26
tthebst <gretler.tim@gmail.com>
2019-10-12 23:55:17 +0200

    reset all added

--- reset_all.sh
+++ reset_all.sh
@@ -10,8 +10,8 @@ echo -n 'gretler.tim@gmail.com' > /home/pi/username.txt
 echo -n '$5' > /home/pi/api_token.txt
 echo -n '$6' > /home/pi/aws_id.txt
 echo -n '$7' > /home/pi/aws_secret.txt
-kubectl create secret generic cloudfare-dns --namespace=kube-system --from-file=/home/pi/username.txt --from-file=/home/pi/api_token.txt
-kubectl create secret generic aws --namespace=kube-system --from-file=/home/pi/aws_id.txt --from-file=/home/pi/aws_secret.txt
+kubectl create secret generic cloudfare-dns  --from-file=/home/pi/username.txt --from-file=/home/pi/api_token.txt
+kubectl create secret generic aws --from-file=/home/pi/aws_id.txt --from-file=/home/pi/aws_secret.txt
 kubectl apply -f traefik_deamon.yaml
 kubectl apply -f traefik_service.yaml
 kubectl apply -f traefik_config.yaml
