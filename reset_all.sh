dbdf02df45890a41a92de51fc0f0e6bb259dad02
tthebst <gretler.tim@gmail.com>
2019-10-13 13:09:00 +0200

    reset all added

--- reset_all.sh
+++ reset_all.sh
@@ -7,9 +7,9 @@ kubectl apply -f https://raw.githubusercontent.com/containous/traefik/v1.7/examp
 
 #setup secets and deploy traefik
 echo -n 'gretler.tim@gmail.com' > /home/pi/username.txt
-echo -n '$5' > /home/pi/api_token.txt
-echo -n '$6' > /home/pi/aws_key.txt
-echo -n '$7' > /home/pi/aws_key_access.txt
+echo -n $1 > /home/pi/api_token.txt
+echo -n $2 > /home/pi/aws_key.txt
+echo -n $3 > /home/pi/aws_key_access.txt
 kubectl create secret generic cloudfare-dns  --from-file=/home/pi/username.txt --from-file=/home/pi/api_token.txt
 kubectl create secret generic aws --from-file=/home/pi/aws_key.txt --from-file=/home/pi/aws_key_access.txt
 kubectl apply -f traefik_deamon.yaml
