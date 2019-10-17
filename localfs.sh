50697152203729f2cf825ffc251e09759b99b400
tthebst <gretler.tim@gmail.com>
2019-10-17 16:45:53 +0200

    check cronjob

--- localfs.sh
+++ localfs.sh
@@ -5,7 +5,7 @@
 
 scp ./exports 192.168.1.97:~/
 #connect to worker1 and add localfs to exports
-ssh 192.168.1.97 'sudo mount /dev/sda1  /home/pi/localfs;sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs;echo "/dev/sda1  /home/pi/localfs   exfat-fuse rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.97 'mkdir /home/pi/localfs;sudo mount /dev/sda1  /home/pi/localfs;sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs;echo "/dev/sda1  /home/pi/localfs   exfat-fuse rw 0 0" >>sudo /etc/fstab'
 
 
 #install exfat utilities on master and other worker nodes
