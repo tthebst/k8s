befddacd703fbb30fdba309ed564387b3184cfde
tthebst <gretler.tim@gmail.com>
2019-10-17 16:40:40 +0200

    check cronjob

--- README.md
+++ README.md
@@ -114,6 +114,7 @@ ssh -L  localhost:8001:localhost:8001 pi@192.168.1.100
 ### Cronjob
 
 (not working currently)
+This will not work because it is not possible to send mails from a kubernetes pod.
 
 Run following command to start a cronjob which checks if websites are still reachable. If not it will send a email to me. 
 ```
--- localfs.sh
+++ localfs.sh
@@ -5,13 +5,16 @@
 
 scp ./exports 192.168.1.97:~/
 #connect to worker1 and add localfs to exports
-ssh 192.168.1.97 'sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs'
-mkdir ~/localfs  
+ssh 192.168.1.97 'sudo mount /dev/sda1  /home/pi/localfs;sudo apt-get install exfat-fuse exfat-utils;sudo apt install nfs-kernel-server;sudo rm /etc/exports;sudo mv ./exports /etc/exports;sudo exportfs -r;sudo exportfs;echo "/dev/sda1  /home/pi/localfs   exfat-fuse rw 0 0" >>sudo /etc/fstab'
+
+
+#install exfat utilities on master and other worker nodes
+mkdir ~/localfs 
 sudo sudo apt-get install exfat-fuse exfat-utils
 sudo umount ~/localfs
 sudo mount 192.168.1.97:/home/pi/localfs ~/localfs
-echo '192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0' >>sudo /etc/fstab
-ssh 192.168.1.98 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
-ssh 192.168.1.99 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    nfs rw 0 0" >>sudo /etc/fstab'
+echo '192.168.1.97:/home/pi/localfs ~/localfs    exfat-fuse rw 0 0' >>sudo /etc/fstab
+ssh 192.168.1.98 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    exfat-fuse rw 0 0" >>sudo /etc/fstab'
+ssh 192.168.1.99 'sudo sudo apt-get install exfat-fuse exfat-utils;mkdir ~/localfs ;sudo umount ~/localfs;sudo mount 192.168.1.97:/home/pi/localfs ~/localfs;echo "192.168.1.97:/home/pi/localfs ~/localfs    exfat-fuse rw 0 0" >>sudo /etc/fstab'
 mount -t nfs
 sudo cat /etc/fstab
