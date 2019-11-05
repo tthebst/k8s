96bbd9163febabc2991d9d4c9595b3fabdfb5a35
tthebst <gretler.tim@gmail.com>
2019-11-05 20:07:13 +0100

    changed cronjob

---
+++ cronjobs/cronjob.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/usr/bin/curl -Is https:/www.stockbuilder.tk | grep 200 || echo "The site is down" | /usr/bin/mail -v -s "Site is down(stockbuilder.tk)" gretler.tim@gmail.com
+/usr/bin/curl -Is https:/www.timgretler.ch | grep 200 || echo "The site is down" | /usr/bin/mail -v -s "Site is down(timgretler.ch)" gretler.tim@gmail.com
+/usr/bin/curl -Is https://www.speechgroup.ch  | grep 200 || echo "The site is down" | /usr/bin/mail -v -s "Site is down(speechgroup.ch)" gretler.tim@gmail.com
+/usr/bin/curl -Is https:/www.stockbuilder123.ch| grep 200 || echo "The site is down" | /usr/bin/mail -v -s "Site is down(stockbuilder.ch)" gretler.tim@gmail.com
