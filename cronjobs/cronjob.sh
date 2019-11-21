6c0187acc072e13ffe1c840e7d3fdba991bf3b3b
tthebst <gretler.tim@gmail.com>
2019-11-21 17:44:47 +0100

    updated cronjob

--- cronjobs/cronjob.sh
+++ cronjobs/cronjob.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #/usr/bin/curl -Is https:/www.stockbuilder.tk | grep 200 || printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(stockbuilder.tk)\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t 
-/usr/bin/curl -Is https:/www.timgretler.ch | grep 200 || (printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(timgretler.ch)\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t) 
-/usr/bin/curl -Is https://www.speechgroup.ch  | grep 200 || (printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(www.speechgroup.ch )\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t) 
-/usr/bin/curl -Is https:/www.stockbuilder123.ch| grep 200 || (printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(stockbuilder.ch)\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t)
+/usr/bin/curl -Is https:/www.timgretler.ch | grep 200 || { printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(timgretler.ch)\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t }
+/usr/bin/curl -Is https://www.speechgroup.ch  | grep 200 || { printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(www.speechgroup.ch )\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t }
+/usr/bin/curl -Is https:/www.stockbuilder123.ch | grep 200 || { printf "To: gretler.tim@gmail.com\nFrom: RaspberryPi3\nSubject: Site is down(stockbuilder.ch)\n\nThere is somthing wrong. Let's go!\n" | ssmtp -t }
 
